2eefd12f13f1268f152c6f9729cc08a625a03c7a
DevWhkang <devwhkang@gmail.com>
2024-05-30 21:44:28 +0900

    Test. ci script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,5 +2,8 @@
 curl https://mise.jdx.dev/install.sh | sh
 mise install # Installs the version from .mise.toml
 
+echo "mise version: "
+~/.local/bin/mise --version
+
 # Runs the version of Tuist indicated in the .mise.toml file
 mise x tuist generate
