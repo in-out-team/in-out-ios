896d75ed3374f53c629b098674287f035027d4f5
DevWhkang <devwhkang@gmail.com>
2024-05-30 21:54:21 +0900

    Test. ci script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl https://mise.jdx.dev/install.sh | sh
 mise install # Installs the version from .mise.toml
-echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+export PATH="$HOME/.local/bin:$PATH"
 
 # Runs the version of Tuist indicated in the .mise.toml file
 mise x tuist generate
