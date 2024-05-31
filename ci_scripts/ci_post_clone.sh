b5dc7459b9e2bf0161f8a81afde7db03a583ed17
DevWhkang <devwhkang@gmail.com>
2024-05-31 18:56:54 +0900

    Test. ci script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,7 +2,7 @@
 
 curl https://mise.jdx.dev/install.sh | sh
 
-export PATH="$HOME/.local/bin/$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 ~/.local/bin/mise --version
 
 # Installs the version from .mise.toml
