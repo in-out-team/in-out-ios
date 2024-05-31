2766d015e2ee62260787243eef6594dc385c5e66
DevWhkang <devwhkang@gmail.com>
2024-05-31 19:04:00 +0900

    Test. ci script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-
 curl https://mise.jdx.dev/install.sh | sh
+export PATH="$HOME/.local/bin/$PATH" # Installs the tools in .mise.toml in the project root
 
-export PATH="$HOME/.local/bin:$PATH"
 ~/.local/bin/mise --version
+~/.local/bin/mise install # Installs the version from .mise.toml
 
-# Installs the version from .mise.toml
-~/.local/bin/mise install
+if [ "$CI" ]; then
+     echo "Skip shims due to CI"
+else
+     echo "Activating shims for local dev"
+     eval "$(mise activate bash --shims)" # activate shims to enable local use of mise
+fi
 
-# Runs the version of Tuist indicated in the .mise.toml file
-~/.local/bin/mise x -- tuist generate
+mise doctor # verify the output of mise is correct on CI
+
+cd ..
+# ~/.local/bin/mise x -- tuist fetch
+~/.local/bin/mise x -- tuist generate # Generate the Xcode Project using Tuist
